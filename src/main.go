d9a4a0eef6f62b93054beebf96d20289fb7bdd4b
aroario2003 <borkbork1031@gmail.com>
2024-09-24 22:56:02 -0400

    basic flags

--- src/main.go
+++ src/main.go
@@ -3,12 +3,28 @@ package main
 import(
 	"fmt"
 	"runtime"
+	"flag"
 )
 
+// variables used in flags
+var (
+	databaseQuery string
+	gui bool
+)
+
+// cli args are intialized here
+func InitCliArgs() {
+	flag.StringVar(&databaseQuery, "query", "", "query to use for querying the database")
+	flag.BoolVar(&gui, "gui", true, "start the gui")
+	flag.Parse()
+}
+
+// get the current os that go is installed on
 func getOs() string {
 	return runtime.GOOS
 }
 
 func main() {
+	InitCliArgs()
 	fmt.Println(getOs())
 }
