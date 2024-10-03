d95ff19e47a6bdf51991e48a804fbdfc6a2d3503
aroario2003 <borkbork1031@gmail.com>
2024-10-03 13:18:57 -0400

    Total vulnerability score and total time to fix implemented

--- cmd/db_linux.go
+++ cmd/db_linux.go
@@ -13,11 +13,65 @@ import (
 	"log"
 	"net"
 	"strings"
+	"strconv"
 )
 
+// get the amount of rows returned by the query
 func CountRowsReturned(result string) int {
 	rows := strings.Split(result, "\n")
-	return len(rows)-1
+	return len(rows)
+}
+
+// get the total cvss score of all vulnerabilities returned by the query
+func getTotalVulnerabilityScore(result string, rowsCount int) float32 {
+	var totalVulnScore float32
+	var vulnScoreStr string
+	resultsArr := strings.Split(result, "\n")
+
+	for _, result := range resultsArr {
+		resultArr := strings.Split(result, " ")
+
+		if GetSoftware() != "" || len(GetOss()) != 0 {
+			vulnScoreStr = resultArr[1]
+		} else if GetCveNum() != "" || len(GetCveNums()) != 0 {
+			vulnScoreStr = resultArr[0]
+		} 
+
+		vulnScore, err := strconv.ParseFloat(vulnScoreStr, 32)
+		if err != nil {
+			log.Printf("Could not convert string to float32 for total vulnerability score: %v", err)
+		}
+
+		totalVulnScore += float32(vulnScore)
+
+	}
+	totalVulnScore = totalVulnScore / float32(rowsCount)
+	return totalVulnScore
+}
+
+// gets the total time to fix of all vulnerabilities returned by the query
+func getTotalTimeToFix(result string) uint8 {
+	var totalTimeToFix uint8
+	var ttfStr string
+	resultsArr := strings.Split(result, "\n")
+
+	for _, result := range resultsArr {
+		resultArr := strings.Split(result, " ")
+
+		if GetSoftware() != "" || len(GetOss()) != 0 {
+			ttfStr = resultArr[2]
+		} else if GetCveNum() != "" || len(GetCveNums()) != 0 {
+			ttfStr = resultArr[1]
+		}
+
+		ttf, err := strconv.ParseUint(ttfStr, 10, 8)
+		if err != nil {
+			log.Printf("Could not convert string to uint8 for total time to fix: %v", err)
+		}
+
+		totalTimeToFix += uint8(ttf)
+	}
+	return totalTimeToFix
 }
 
 // Takes os or software and returns cve number, vulnerability score and time to fix
--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/aroario2003/cram
 go 1.23.1
 
 require (
+	github.com/Ne0nd0g/npipe v1.1.0
+	github.com/go-sql-driver/mysql v1.8.1
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/Ne0nd0g/npipe v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 )
